ca3f38c4a44c832655ef422d03a6e54cee3f098a
Paul Mason <paul.mason@outlook.com>
2017-03-19 12:53:04 +0200

    Updated to V3.

--- my-element.js
+++ my-element.js
@@ -1,19 +1,39 @@
 class MyElement extends Atom.Element {
     static get is () { return "my-element"; }
     
+    static get observedAttributes() {
+        return ["first-name", "last-name", "age", "foo"];
+    }
+    
+    static get properties() {
+        return {
+            firstName: {
+                value: "Bob",
+                reflectToAttribute: true
+            },
+            
+            lastName: {
+                value: "Smith",
+                reflectToAttribute: true
+            },
+            
+            age: {
+                value: "35"
+            }
+        }
+    }
+    
     get template () {
         return `
             <div>
-                <h3>Hello ${this.person}</h3>
-                <p>This is a custom element</p>
+                <h3>Hello ${this.firstName} ${this.lastName}, you are ${this.age} years old. Hey ${this.foo}</h3>
             </div>
         `;
     }
-   
+    
     constructor () {
         super();
-        this.person = "Bob";
-        this.init();
+        this.age = 47;
     }
 }
  
